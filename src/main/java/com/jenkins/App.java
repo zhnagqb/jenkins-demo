9c78a09911a0ae4d5d02bbe790294f355ae7e51f
zhangqb@git.com <54842585+zhnagqb@users.noreply.github.com>
2021-08-29 12:05:06 +0800

    提交demo代码

---
+++ src/main/java/com/jenkins/App.java
@@ -0,0 +1,16 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: zhangqingbiao
+ * @Date: 2021/8/29 9:52
+ */
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class);
+  }
+}
